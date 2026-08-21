ad08424109713471037bf27867b767c61f4732d8
fersouzaaa <fernandasouzasil06@gmail.com>
2026-08-20 21:27:20 -0300

    feat(configuration): configuration

---
+++ first-spring-app/src/main/java/com/fernanda/first_spring_app/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,14 @@
+package com.fernanda.first_spring_app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+
+    @Bean
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+}
